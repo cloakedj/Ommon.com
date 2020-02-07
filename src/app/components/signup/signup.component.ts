ae0e3dc6f4f9351cc6806c726d4dcce8f8f69ab1
unknown <kanwardhananjaysingh0545@gmail.com>
2020-02-07 11:25:22 +0530

    Updated UI

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.sass']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  toggleMessageCard : boolean = true;
+  signUpForm : FormGroup  = this._fb.group({
+    name : ['',[
+      Validators.required,
+      Validators.maxLength(30)
+    ]],
+    pNumber : ['',[
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern(/^[0-9]+$/)
+    ]],
+    email : ['',[
+      Validators.email,
+      Validators.required
+    ]],
+    dob : ['',[Validators.required]],
+    collegeName : ['', [Validators.required,
+    Validators.pattern(/^[A-Za-z]+$/)]],
+    courseName : ['',[
+      Validators.required,
+      Validators.pattern(/^[A-Za-z]+$/)
+    ]],
+    policyAgreement : ['',
+    [Validators.requiredTrue]
+  ]
+  });
+  constructor(private _fb : FormBuilder) {  
   }
 
+  ngOnInit() {
+   
+  }
+  uploadStudentInformation(data : any){
+
+  }
+  get name(){ return this.signUpForm.get("name")};
+  get pNumber(){ return this.signUpForm.get("pNumber")};
+  get email(){ return this.signUpForm.get("email")};
+  get dob(){ return this.signUpForm.get("dob")};
+  get collegeName(){ return this.signUpForm.get("collegeName")};
+  get courseName(){ return this.signUpForm.get("courseName")};
+  get policyAgreement(){ return this.signUpForm.get("policyAgreement")};
 }
